5f3be4d4c8236e4538e0447033c2eca7c89ab558
Nikolay Patonov <72614967+patonov@users.noreply.github.com>
2021-10-25 15:04:22 +0300

    Add files via upload

---
+++ MongoDriver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = client.GetDatabase("SoftUni");
+            var collection = database.GetCollection<BsonDocument>("Students");
+            var studentDoc = new BsonDocument {
+                { "Name", "Pesho" }
+            };
+            collection.InsertOne(studentDoc);
+
+            var students = collection.Find<BsonDocument>(new BsonDocument()).ToList();
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
